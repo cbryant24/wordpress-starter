7067de0ba48823bd253ad577e78ce6f60652e4a3
Chris <chrs.bryant@gmail.com>
2020-10-21 00:31:07 -0700

    added config changes for wp-config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,8 @@
 ############################################
 
 set :stage, :production
-set :stage_domain, "[add name for production]"
-server "[add server name]", user: "[add deploy user]", roles: %w(web app db)
+set :stage_domain, "chris-s-bryant.com"
+server "chris-s-bryant.com", user: "deploy", roles: %w(web app db)
 set :deploy_to, "/var/www/html"
 set :shared_directory, "/var/www/html/shared"
 set :current_directory, "/var/www/html/current"
